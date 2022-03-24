7146cde509825234c41f4f88ffbd952db94ce0eb
David Richey <darichey1@gmail.com>
2022-03-24 12:31:22 -0500

    Use sqlite db in backend

--- functions/services/section/get/handler.js
+++ functions/services/section/get/handler.js
@@ -2,19 +2,19 @@ const { getConnectionManager, createConnection } = require('typeorm');
 const { CatalogNumber, Grades, Professor, Section, Semester, Subject } = require("utd-grades-models");
 const respond = require('../../../utils/respond');
 const get = require('./index');
+const fs = require("fs/promises");
 
 let con;
 
 module.exports.handler = async (event, context) => {
 // TODO deduplication between here and other handler
   if (!con) {
+    const data = await fs.readFile("../data/utdgrades.sqlite3");
+
     con = await createConnection({
       name: "getSection",
-      type: "postgres",
-      host: "localhost",
-      username: "postgres",
-      database: "utdgrades",
-      synchronize: true,
+      type: "sqljs",
+      database: data,
       entities: [CatalogNumber, Grades, Professor, Section, Semester, Subject],
       // logging: true // useful for debugging
     });
