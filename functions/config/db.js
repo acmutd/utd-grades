7dbb9f9b43ad282d1452ced60336ccf4430d8fe5
David Richey <darichey1@gmail.com>
2022-03-24 12:31:22 -0500

    Use sqlite db in backend

--- functions/config/db.js
+++
@@ -1,8 +0,0 @@
-const keys = require('./keys');
-
-module.exports = {
-  dbName: keys.get('DB_NAME'),
-  dbUser: keys.get('DB_USER'),
-  dbPass: keys.get('DB_PASS'),
-  dbHost: keys.get('DB_HOST'),
-};
