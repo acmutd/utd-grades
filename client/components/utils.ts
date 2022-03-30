937f87c947ae73fc1ab0035230dd206bfff7d73a
David Richey <darichey1@gmail.com>
2022-03-29 21:16:04 -0500

    Fix tests

--- client/components/utils.ts
+++ client/components/utils.ts
@@ -1,4 +1,4 @@
 export function splitName(name: string): [first: string, last: string] {
-  const space = name.indexOf(' ');
+  const space = name.lastIndexOf(' ');
   return [name.substring(0, space), name.substring(space + 1)];
 }
